25fe7499f6ef6fb2d3940883736b4b862a4c4025
ExtEOi <11038937+ExtEOi@users.noreply.github.com>
2020-06-27 14:55:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,21 +18,25 @@ http.createServer(function(req, res){
       if(!data){
         res.end("No post data");
         res.end();
+        return;
       }
       var dataObject = querystring.parse(data);
       console.log("post:" + dataObject.type);
       if(dataObject.type == "wake"){
         console.log("Woke up in post");
         res.end();
+        return;
       }
       if(dataObject.hash === undefined || dataObject.nonce === undefined){
         console.log("undefined hash");
         res.end();
+        return;
       }else{
         let serverHash = crypto.createHash('sha256').update(password + Math.floor(dataObject.nonce)).digest('hex');
         if(String(dataObject.hash) != serverHash){
           console.log("invalid hash");
           res.end();
+          return;
         }else{
           console.log("nonce:" + Math.floor(dataObject.nonce));
           console.log("hash ok");
